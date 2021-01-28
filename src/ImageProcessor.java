97698eec9ff64589bbeeaf795fcbc993c67a904a
Alexandr Zhalnerchik <alexzhalnerchikz@mail.ru>
2021-01-28 21:07:58 +0300

    fix indents at ImageProcessor

--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -2,17 +2,18 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class ImageProcessor {
-ArrayList<IImageEditor>filters=new ArrayList<>();
+ ArrayList<IImageEditor>filters=new ArrayList<>();
 
-   public void AddFilter(IImageEditor editor){
+    public void AddFilter(IImageEditor editor){
      filters.add(editor);
      }
 
-  public BufferedImage ApplyEditors(BufferedImage original){
-   for (IImageEditor edit:filters){
-       original=edit.Apply(original);
-   }
-   return original;
-   }
+
+     public BufferedImage ApplyEditors(BufferedImage original){
+       for (IImageEditor edit:filters){
+         original=edit.Apply(original);
+       }
+      return original;
+     }
 
 }

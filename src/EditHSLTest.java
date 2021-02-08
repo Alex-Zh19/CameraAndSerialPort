2308eb123edd6c9861957a8acd509bc8588a6eae
Alexandr Zhalnerchik <alexzhalnerchikz@mail.ru>
2021-02-08 18:50:46 +0300

    EditHSL test added

---
+++ src/EditHSLTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.awt.image.BufferedImage;
+
+import static org.junit.Assert.*;
+
+public class EditHSLTest {
+
+    @Test
+    public void apply() {
+        IImageEditor filter=new EditHSL();
+        BufferedImage image = new BufferedImage(200, 200, BufferedImage.TYPE_INT_RGB);
+        BufferedImage dstImage=filter.Apply(image);
+
+        Assert.assertFalse(imageEqual(image,dstImage));
+    }
+    boolean imageEqual(BufferedImage im1,BufferedImage im2){
+        for(int i=0;i<im1.getWidth();++i){
+            for(int j=0;j<im1.getHeight();++j){
+                int im1Num=im1.getRGB(i,j);
+                int im2Num=im2.getRGB(i,j);
+                if(im1Num!=im2Num){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
